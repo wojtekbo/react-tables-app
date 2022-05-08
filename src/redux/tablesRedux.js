1e484be9125870a7ee4ebfe27a05f1b09cb9e3c2
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2022-05-08 22:32:33 +0200

    Fix heroku configuration

--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -1,3 +1,5 @@
+import {API_URL} from '../config.js';
+
 //selectors
 export const getAllTables = ({tables}) => tables;
 export const getTableById = ({tables}, id) => tables.filter(table => table.id === id)[0];
@@ -10,14 +12,14 @@ const UPDATE_TABLES = createActionName('UPDATE_TABLES');
 export const updateTables = payload => ({type: UPDATE_TABLES, payload});
 export const fetchTables = () => {
   return dispatch => {
-    fetch('http://localhost:3131/api/tables')
+    fetch(API_URL + '/tables')
       .then(response => response.json())
       .then(tables => dispatch(updateTables(tables)));
   };
 };
 
 export const editTable = table => {
-  const url = 'http://localhost:3131/tables/' + table.id;
+  const url = API_URL + '/tables/' + table.id;
   const options = {
     method: 'PUT',
     headers: {
